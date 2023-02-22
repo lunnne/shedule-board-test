84551044e19931e6d95eee55e0e3a305cd77a544
Jin <munjin1340@gmail.com>
2023-02-22 23:37:09 +0100

    select function

--- src/components/TeacherCard.jsx
+++ src/components/TeacherCard.jsx
@@ -6,7 +6,7 @@ export default function TeacherCard({ items }) {
   return (
     <>
       {items.map((item) => <div className= 'flex flex-col justify-center items-center' >
-        <img className='w-36 h-36 rounded-full' src={item.url} alt={item.name}/>
+        <img className='w-36 h-36 rounded-full' src={require(`../asset/${item.name}.png`)} alt={item.name}/>
         <p className='text-xl'>{item.name}</p>
       </div> )}
     </>
